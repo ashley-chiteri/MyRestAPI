efc297c9e77921dbc1a65a899a74328adaba0b8a
Ashley Chiteri <ochamiashley97@gmail.com>
2024-05-24 14:40:51 +0300

    Added a functionality to upload images

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,17 +1,49 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const mutler = require('multer');
+
+//image storage configuration
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+//filtering images 
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        //accepting a file
+        cb(null, true);
+    } else {
+        //reject a file
+        cb(null, false);
+    }
+
+}
+const upload = mutler({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
 
 const Product = require('../models/product');
+const multer = require('multer');
 
 router.get('/', (req, res, next) => {
-    Product.find().select('name price _id').exec().then(docs => {
-        const response ={
+    Product.find().select('name price _id productImage').exec().then(docs => {
+        const response = {
             count: docs.length,
             products: docs.map(doc => {
                 return {
                     name: doc.name,
                     price: doc.price,
+                    productImage: doc.productImage,
                     _id: doc._id,
                     request: {
                         type: 'GET',
@@ -23,17 +55,18 @@ router.get('/', (req, res, next) => {
         res.status(200).json(response);
     }).catch(err => {
         console.log(err);
-        res.status(500).json({error: err});
+        res.status(500).json({ error: err });
     })
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('productImage'), (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price: req.body.price
+        price: req.body.price,
+        productImage: req.file.path
     });
-    product.save().then(result =>{
+    product.save().then(result => {
         console.log(result);
         res.status(201).json({
             message: 'Created a product',
@@ -48,42 +81,43 @@ router.post('/', (req, res, next) => {
             }
         });
     })
-    .catch(err =>{ 
-        console.log(err);
-        res.status(500).json({
-            error: err
-        })
-    });
-    
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
+
 });
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    Product.findById(id).select('name price _id').exec().then(doc => {
-        console.log("From database",doc);
-        if(doc){
+    Product.findById(id).select('name price _id productImage').exec().then(doc => {
+        console.log("From database", doc);
+        if (doc) {
             res.status(200).json({
                 product: doc,
-            request: {
-                type: 'GET',
-                url: 'http://localhost:3000/products'
-            }});
-        }else{
-            res.status(404).json({message: 'No valid entry found for provided ID'});
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products'
+                }
+            });
+        } else {
+            res.status(404).json({ message: 'No valid entry found for provided ID' });
         }
     }).catch(err => {
         console.log(err);
-        res.status(500).json({error: err});
+        res.status(500).json({ error: err });
     })
 })
 
 router.patch('/:productId', (req, res, next) => {
     const id = req.params.productId;
     const updateOps = {};
-    for (const ops of req.body){
+    for (const ops of req.body) {
         updateOps[ops.propName] = ops.value
     }
-    Product.updateOne({ _id: id},{ $set: updateOps}).exec().then(result => {
+    Product.updateOne({ _id: id }, { $set: updateOps }).exec().then(result => {
         res.status(200).json({
             message: 'Product updated',
             request: {
@@ -93,24 +127,24 @@ router.patch('/:productId', (req, res, next) => {
         });
     }).catch(err => {
         console.log(err);
-        res.status(500).json({error: err});
+        res.status(500).json({ error: err });
     })
 });
 
 router.delete("/:productId", (req, res, next) => {
     const id = req.params.productId;
-    Product.deleteOne({ _id: id}).exec().then(result => {
+    Product.deleteOne({ _id: id }).exec().then(result => {
         res.status(200).json({
             message: 'Product deleted',
             request: {
                 type: 'POST',
                 url: 'http://localhost:3000/products/',
-                body: {name : 'String', price: 'Number'}
+                body: { name: 'String', price: 'Number' }
             }
         });
     }).catch(err => {
         console.log(err);
-        res.status(500).json({error: err});
+        res.status(500).json({ error: err });
     })
 });
 
--- app.js
+++ app.js
@@ -16,6 +16,7 @@ mongoose.connect('mongodb+srv://shop-node:'+process.env.DB_PASSWRD+'@node-rest-s
 mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
